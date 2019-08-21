0e7344f7532ebce6b7cd8db988e144ff34214334
Julian Enochs <julianenochs@yahoo.com>
2019-08-21 13:44:09 -0600

    Update filepaths and script tags

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,9 @@
+let randomId = Math.floor(Math.random() * 50 + 1)
+let userRepository = new UserRepository(userData)
+// let userInfo = userRepository.getUserData(randomId)
+// let user = new User(userInfo)
+
+
 $(document).ready(() => {
     $('#splash-page-js').show();
     $('#main-page-js').hide();
@@ -9,4 +15,5 @@ $(document).ready(() => {
         $('#main-page-js').fadeIn(1000);
         $('#header-js').show();
     }
+    $('#user-info__display-js').text(user.name)
 });
